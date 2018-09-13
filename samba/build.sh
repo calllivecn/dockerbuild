5abddbdb7d01b382d6fd4869a1f927549219ba8a
calllivecn <calllivecn@gmail.com>
2018-09-14 01:05:24 +0800

    修改：build.sh 修改：dockerfile 修改：smb.conf
    做了一些，没有的修改。。。。

--- samba/build.sh
+++ samba/build.sh
@@ -2,14 +2,13 @@
 # date 2018-09-13 20:48:25
 # author calllivecn <c-all@qq.com>
 
+set -x
+
 if [ -z $1 ];then
 	echo "需要给一个samba密码"
 	exit 1
-elif [ -n $1 ];then
-	sed -i "s#PW#${1}" dockerfile
-	docker build -t samba .
-
 else
-	echo "Usage: sh build.sh samba_password"
+	sed -i "s#PW#${1}#" dockerfile
+	docker build -t samba .
 fi
 
