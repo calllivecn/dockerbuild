9b2d4296ec3feec278a6a98b4c6da47b859ef02f
calllivecn <calllivecn@outlook.com>
2018-09-16 01:17:23 +0800

    修改：     samba/.dockerignore 修改：     samba/build.sh

--- samba/build.sh
+++ samba/build.sh
@@ -5,8 +5,22 @@
 DEPENDS=ubuntu
 . ../libbuild-depends.sh
 
-if [ -n $NO_CACHE ];then
-	docker build --no-cache -t ${IMAGE_NAME} .
-else
-	docker build -t ${IMAGE_NAME} .
+if [ -z "$1" ];then
+	echo "请给出你的samba密码。"
+	exit 1
 fi
+
+SED=dockerfile-sed
+
+sed "s#PW#${1}#" dockerfile > ${SED}
+
+if [ -n $NO_CACHE ];then
+	docker build --no-cache -t ${IMAGE_NAME} -f ${SED} .
+else
+	docker build -t ${IMAGE_NAME} -f ${SED} .
+fi
+
+rm -rf ${SED}
+
+echo "samba username: root"
+echo "samba password: ${1}"
