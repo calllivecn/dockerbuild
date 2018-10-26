a82d62e0955cd94bdbf1d9823c12f995fc04df38
calllivecn <c-all@qq.com>
2018-10-26 11:55:59 +0800

    新文件：anyconnect/README.md 新文件：anyconnect/build.sh 新文件：anyconnect/dockerfile 新文件：anyconnect/run.sh

---
+++ anyconnect/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# date 2018-10-26 10:18:32
+# author calllivecn <c-all@qq.com>
+
+sudo xhost +
+
+sudo docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix docker-run-gui /opt/cisco/anyconnect/bin/vpnui
