37864ac529efdc7599f03077d04c2725fd98088f
calllivecn <calllivecn@outlook.com>
2019-05-17 20:57:12 +0800

    修改：     samba/smb.conf 修改：     update-images.sh

--- update-images.sh
+++ update-images.sh
@@ -2,23 +2,42 @@
 # date 2018-08-24 23:01:51
 # author calllivecn <c-all@qq.com>
 
-if [ -z "$1" ];then
-	echo "需要你的docker registry前缀！！！(如：registry.cn-hangzhou.aliyuncs.com/calllivecn)"
-	exit 1
-fi
 
 prefix_docker_registry="$1"
 
 cd $(dirname ${0})
 
-for files in *;
-do
-	if [ -d ${files} ] && [ -f ${files}/dockerfile ];then
-		pushd ${files}
-		docker build -t ${files} . \
-		&& docker tag ${files} ${prefix_docker_registry}/${files} \
-		&& docker push ${prefix_docker_registry}/${files} \
-		&& docker rmi ${prefix_docker_registry}/${files}
-		popd
-	fi
-done
+build_push(){
+	for files in *;
+	do
+		if [ -d ${files} ] && [ -f ${files}/dockerfile ];then
+			pushd ${files}
+			docker build -t ${files} . \
+			&& docker tag ${files} ${prefix_docker_registry}/${files} \
+			&& docker push ${prefix_docker_registry}/${files} \
+			&& docker rmi ${prefix_docker_registry}/${files}
+			popd
+		fi
+	done
+}
+
+build(){
+
+	for files in *;
+	do
+		if [ -d ${files} ] && [ -f ${files}/dockerfile ];then
+			pushd ${files}
+			docker build -t ${files} . 
+			popd
+		fi
+	done
+
+}
+	#echo "需要你的docker registry前缀！！！(如：registry.cn-hangzhou.aliyuncs.com/calllivecn)"
+	#exit 1
+
+if [ -n "$1" ];then
+	build_push
+else
+	build
+fi
