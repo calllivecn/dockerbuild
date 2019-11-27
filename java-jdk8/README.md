373c5a0011acb3b674ca9475a3f92935fef8c156
calllivecn <calllivecn@outlook.com>
2019-11-27 11:47:16 +0800

    新文件：java-jdk8 新文件：java-maven

---
+++ java-jdk8/README.md
@@ -0,0 +1,8 @@
+# build java-jdk8
+
+- 1.把下载的java jdk8包解压放到当前目录重命名为jdk8/
+
+- 2.
+```shell
+bash build.sh
+```
---
+++ java-jdk8/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# date 2019-11-27 11:11:36
+# author calllivecn <c-all@qq.com>
+
+DEPENDS=ubuntu-base
+. ../libbuild-depends.sh
+
+
+if [ -n $NO_CACHE ];then
+	docker build --no-cache -t ${IMAGE_NAME} .
+else
+	docker build -t ${IMAGE_NAME} .
+fi
