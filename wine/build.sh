770171058fb7f064044a975f7ef28e3893a09ea0
calllivecn <c-all@qq.com>
2020-07-03 00:37:07 +0800

    wine 不能用root运行，

--- wine/build.sh
+++ wine/build.sh
@@ -13,26 +13,20 @@ FROM ${DEPENDS}:latest
 
 LABEL author="ZhangXu <c-all@qq.com>" repositories="https://github.com/calllivecn/dockerbuild/"
 
-RUN sed -i -e "s#archive.ubuntu.com#mirrors.aliyun.com#g" -e "s#security.ubuntu.com#mirrors.aliyun.com#g" /etc/apt/sources.list \
-&& apt -y update \
-&& dpkg --add-architecture i386 \
-&& apt -y install ca-certificates gnupg2 iproute2 vim bash-completion less iputils-ping telnet wget curl git \
-&& wget -O- https://dl.winehq.org/wine-builds/winehq.key| apt-key add - \
-&& echo "deb https://dl.winehq.org/wine-builds/ubuntu/ $(grep CODENAME /etc/lsb-release |cut -d'=' -f2) main" >> /etc/apt/sources.list.d/wine.list \
-&& apt -y update \
-&& DEBIAN_FRONTEND=noninteractive apt -y install --install-recommends winehq-stable \
-&& ln -vsf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
-&& dpkg-reconfigure -f noninteractive tzdata  \
-&& apt clean \
-&& rm -rf /var/lib/apt/lists
+ARGS APT_PROXY
+
+COPY containers/ /tmp/
+
+RUN bash /tmp/containers/RUN.sh \
+&& mv /tmp/contaners/entrypoint.sh / \
+&& rm -rf /tmp/containers/ 
+
+WORKDIR /home/wine
+
+CMD ["/entrypoint.sh"]
 
 EOF
 
-# 设置容器里的时区
-
-## && DEBIAN_FRONTEND=noninteractive apt install -y tzdata \
-## && ln -vsf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
-## && dpkg-reconfigure -f noninteractive tzdata 
 
 if [ -n $NO_CACHE ];then
 	docker build --no-cache -t ${IMAGE_NAME} .
