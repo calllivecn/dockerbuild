e54ae30fc680e0651f7fc37b121076e7d777748c
calllivecn <c-all@qq.com>
2020-07-03 21:54:41 +0800

    手动执行（是阿里源的锅！)

--- wine/README.md
+++ wine/README.md
@@ -22,3 +22,15 @@ winecfg
 ```bash
 bash build.sh
 ```
+
+
+## build可能的问题
+
+```
+阿里云的源总会(也不确定是阿里云的锅。)：
+E: Failed to fetch http://mirrors.aliyun.com/ubuntu/pool/main/libv/libvdpau/vdpau-driver-all_1.3-1ubuntu2_amd64.deb Undetermined Error [IP: 113.96.109.246 80]
+E: Unable to fetch some archives, maybe run apt-get update or try with --fix-missing?
+				    
+目前解决方式，多执行几次: apt install winehq-stable
+不行的话: 可以手动执行构建过程，完成后，docker commit 
+```
--- wine/containers/RUN.sh
+++ wine/containers/RUN.sh
@@ -10,8 +10,7 @@ else
 	echo "没走代理: $APT_PROXY" >&2
 fi
 
-sed -i -e "s#archive.ubuntu.com#mirrors.aliyun.com#g" \
-	-e "s#security.ubuntu.com#mirrors.aliyun.com#g" /etc/apt/sources.list
+sed -i -e "s#archive.ubuntu.com#mirrors.aliyun.com#g" -e "s#security.ubuntu.com#mirrors.aliyun.com#g" /etc/apt/sources.list
 
 apt -y update
 
@@ -24,7 +23,7 @@ locale-gen en_US.UTF-8 zh_CN.UTF-8
 
 wget -O- https://dl.winehq.org/wine-builds/winehq.key| apt-key add -
 
-echo "deb https://dl.winehq.org/wine-builds/ubuntu/ $(grep CODENAME /etc/lsb-release |cut -d'=' -f2) main" >> /etc/apt/sources.list.d/wine.list \
+echo "deb https://dl.winehq.org/wine-builds/ubuntu/ $(grep CODENAME /etc/lsb-release |cut -d'=' -f2) main" >> /etc/apt/sources.list.d/wine.list
 
 apt -y update 
 
@@ -39,6 +38,8 @@ fi
 # 创建普通用户用来启动wine
 
 useradd -m -s /bin/bash wine
+
+echo "export LANG=zh_CN.UTF-8" >> /home/wine/.profile
 
 # 设置容器里的时区
 ln -vsf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime 
