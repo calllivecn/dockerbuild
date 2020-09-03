d746a83942615f067ddad1ea683b2f35677e7631
calllivecn <c-all@qq.com>
2020-09-03 10:33:21 +0800

    samba 改为启动时添加UID，和设置密码。

---
+++ samba/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+UID=${UID:-0}
+#gid=${GID:-0}
+
+USERNAME="samba"
+
+if [ -z "$PW" ];then
+	echo "passwd is required."
+	exit 1
+fi
+
+LOGLEVEL=${LOGLEVEL:-1}
+
+adduser -D -u $UID $USERNAME
+
+#smbpasswd -a $USERNAME
+
+echo -e "${PW}\n${PW}" |pdbedit -t -a -u $USERNAME
+
+exec smbd -FS --no-process-group -d ${LOGLEVEL} -s /etc/samba/smb.conf -p 445
