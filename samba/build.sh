ada602a9dfebb239fa93034caec7df5318ab3dc4
calllivecn <calllivecn@outlook.com>
2020-09-03 10:33:21 +0800

    samba 改为启动时添加UID，和设置密码。

--- samba/build.sh
+++ samba/build.sh
@@ -5,19 +5,10 @@
 DEPENDS=alpine-py3
 . ../libbuild-depends.sh
 
-if [ -z "$1" ];then
-	echo "请给出你的samba密码。"
-	exit 1
-else
-	PW="$1"
-fi
 
 if [ -n $NO_CACHE ];then
-	docker build --no-cache --build-arg "$PW" -t ${IMAGE_NAME} .
+	docker build --no-cache -t ${IMAGE_NAME} .
 else
 	docker build -t ${IMAGE_NAME} .
 fi
 
-
-echo "samba username: root"
-echo "samba password: ${PW}"
