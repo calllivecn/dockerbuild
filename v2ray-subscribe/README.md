0e66c71917f1f9679f253735b884b7aa807d7c04
calllivecn <calllivecn@outlook.com>
2022-05-21 08:36:18 +0800

    1. fix subprocess.Popen(), .terminate(), .kill() 2. 添加流量查询

--- v2ray-subscribe/README.md
+++ v2ray-subscribe/README.md
@@ -1,11 +1,14 @@
 # 给 搬瓦工服务 v2ray 添加自动更新订阅
 
-- v2ray: https://github.com/v2ray/v2ray-core/releases
+- v2ray: https://github.com/v2fly/v2ray-core/releases
 
 - 启动方式：
 
 ```shell
+# 可选参数 V2RAY_PATH: v2ray 目录
 podman run -d --name v2ray -p 9999:9999 -p 10000:10000 -e SERVER_URL=https://v2ray.server.com/
 ro
 podman run -d --name v2ray -p <http_proxy port>:9999 -p <socks5_proxy port>:10000 -e SERVIER_URL=<$your subscription address> -e UPDATE_INTERVAL=<unit hour>
+or
+podman run -d --name v2ray -p <http_proxy port>:9999 -p <socks5_proxy port>:10000 -e SERVIER_URL=<$your subscription address> -e UPDATE_INTERVAL=<unit hour> -e API_COUNTER=<api check>
 ```
--- v2ray-subscribe/entrypoint.py
+++ v2ray-subscribe/entrypoint.py
@@ -11,14 +11,16 @@ import base64
 import pprint
 import socket
 import logging
+import argparse
 import subprocess
+from pathlib import Path
 from urllib import request
 
 V2RAY_CONFIG_JSON = {
     "log": {
         "loglevel": "info",
-        "access": "/v2ray-access.logs",
-        "error": "/v2ray-error.logs"
+        "access": "access.logs",
+        "error": "error.logs"
     },
     "inbounds": [
         {
@@ -61,6 +63,21 @@ V2RAY_CONFIG_JSON = {
                     }
                 ]
             }
+        },
+        {
+            "protocol": "shadowsocks",
+            "settings":{
+                "servers": [
+                    {
+                        "address": "{SS_ADDR}",
+                        "port": "{SS_PORT}",
+                        "method": "aes-256-gcm",
+                        "password": "{PW}",
+                        "ota": False,
+                        "level": 0
+                    }
+                ]
+            }
         }
     ]
 }
@@ -71,7 +88,7 @@ def getlogger(level=logging.INFO):
     stream = logging.StreamHandler(sys.stdout)
     stream.setFormatter(fmt)
 
-    fp = logging.FileHandler("v2ray-manager.logs")
+    fp = logging.FileHandler("manager.logs")
     fp.setFormatter(fmt)
 
     logger = logging.getLogger("AES")
@@ -87,22 +104,19 @@ logger = getlogger()
 def runtime(prompt):
     def decorator(func):
         def warp(*args, **kwarg):
-            logger.info(f"更新节点信息")
             t1 = time.time()
             result = func(*args, **kwarg)
             t2 = time.time()
-            logger.info(f"{prompt}  运行时间: {t2-t1}/s")
+            logger.info(f"{prompt} 运行时间: {t2-t1}/s")
             return result
         return warp
     return decorator
 
 
-@runtime("get subscription")
 def get(url):
     req = request.Request(url, headers={"User-Agent": "curl/7.68.0"}, method="GET")
     data = request.urlopen(req)
     context = data.read()
-    logger.info(f"server url result: {context}")
     return context
 
 
@@ -123,12 +137,35 @@ SERVER_URL = getenv("SERVER_URL")
 logger.debug(f"SERVER_URL: {SERVER_URL}")
 
 # v2ray path
-V2RAY_PATH = os.environ.get("V2RAY_PATH", "/v2ray")
+V2RAY_PATH = Path(os.environ.get("V2RAY_PATH", "/v2ray"))
 
 # 多久更新一次 unit hour
 UPDATE_INTERVAL = int(os.environ.get("UPDATE_INTERVAL", "8"))
 
+# 查看当前流量使用情况，和到期时间。
+def check_subscription():
+    API = os.environ.get("API_COUNTER")
+    if API is not None:
 
+        try:
+            result = get(API)
+        except Exception:
+            return
+
+        j = json.loads(result) 
+
+        total = j["monthly_bw_limit_b"]
+        use = j["bw_counter_b"]
+        reset_day = j["bw_reset_day_of_month"]
+        
+        total_GB = round(total/(1<<30), 2)
+        use_GB = round(use/(1<<30), 2)
+        percentage = round(use/total*100, 2)
+
+        logger.info(f"当前总共流量 {total_GB}G，当前流量使用: {use_GB}GB 当前已使用百分比： {percentage}% 流量重置日期：{reset_day}")
+
+
+@runtime("get subscription")
 def getsubscription(context):
     proxy_urls = base64.b64decode(context)
     proxys = {}
@@ -136,32 +173,51 @@ def getsubscription(context):
     for url in proxy_urls.split(b"\n"):
 
         if url.startswith(b"ss://"):
-            logger.info(f"目前先不支持ss: {url}")
-            # ss = base64.b64decode(url[5:].encode("ascii")).decode("ascii")
-            # if proxys.get("ss"):
-            #     proxys["ss"].append(ss)
-            # else:
-            #     proxys["ss"] = [ss]
+            # logger.warning(f"目前先不支持ss: {url}")
+            ss = base64.b64decode(url[5:]).decode("ascii")
+            if proxys.get("ss"):
+                proxys["ss"].append(ss)
+            else:
+                proxys["ss"] = [ss]
+
+
+        # elif url.startswith(b"ssr://"):
+        #     logger.debug(f"ssr 协议")
+
+        #     try:
+        #         vmess = base64.b64decode(check_b64(url[6:]))
+        #     except Exception:
+        #         logger.error(f"Error: base64.b64decode() --> {url}")
+        #         continue
+
+        #     # 加入到proxy 
+        #     if proxys.get("ssr"):
+        #         proxys["ssr"].append(json.loads(vmess))
+        #     else:
+        #         proxys["ssr"] = [json.loads(vmess)]
+
 
         elif url.startswith(b"vmess://"):
             try:
                 vmess = base64.b64decode(check_b64(url[8:]))
             except Exception:
-                logger.info(f"Error: base64.b64decode() --> {url}")
+                logger.error(f"Error: base64.b64decode() --> {url}")
                 continue
 
+            # 加入到proxy 
             if proxys.get("vmess"):
                 proxys["vmess"].append(json.loads(vmess))
             else:
                 proxys["vmess"] = [json.loads(vmess)]
 
         else:
-            logger.warn(f"未知协议: {url}", file=sys.stderr)
+            logger.warning(f"未知协议: {url}")
 
     return proxys
 
 
 # 目前先只支持vmess
+@runtime("测试链接速度")
 def test_connect_speed(vmess_list):
     score = []
     for vmess in vmess_list:
@@ -200,28 +256,54 @@ def updatecfg(vmess_json):
 
     V2RAY_CONFIG_JSON["outbounds"] = outbounds
 
-    with open(os.path.join(V2RAY_PATH, "config.json"), "w") as f:
+    with open(V2RAY_PATH / "config.json", "w") as f:
         f.write(json.dumps(V2RAY_CONFIG_JSON, ensure_ascii=False, indent=4))
 
 
 def v2ray(config):
-    # v4.31.0 
+
+    # v4.xx.x
     #subproc = subprocess.Popen(f"/v2ray/v2ray -config {config}".split())
 
-    # v5.0
-    subproc = subprocess.Popen(f"/v2ray/v2ray run".split())
+    # v5.0.x
+    p = V2RAY_PATH / "v2ray"
+    if p.exists():
+        subproc = subprocess.Popen(f"{p} run".split())
+    else:
+        subproc = subprocess.Popen(f"/v2ray/v2ray run".split())
+
     return subproc
 
 
 def reboot(subproc, config):
-    subproc.terminate()
+
+    for i in range(5):
+        recode = subproc.terminate()
+        logger.info(f"terminate() retry {i}/5")
+        if recode is None:
+            time.sleep(1)
+        else:
+            logger.info(f"terminate() ok")
+            break
+
+    # 如果subprc 是结束，send_signal() 什么也不做。
+    subproc.send_signal(9)
+
     new_subproc = v2ray(config)
     return new_subproc
 
 
 def main():
 
-    logger.info(f"第{UPDATE_INTERVAL}小时更新节点信息")
+    parse = argparse.ArgumentParser()
+    parse.add_argument("--debug", action="store_true", help=argparse.SUPPRESS)
+
+    args = parse.parse_args()
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
+    logger.info(f"第 {UPDATE_INTERVAL} 小时更新节点信息")
 
     v2ray_config = os.path.join(V2RAY_PATH, "config.json")
 
@@ -235,6 +317,8 @@ def main():
             logger.info("server 信息没更新，不重启")
 
         else:
+            logger.info(f"server url result: {server_info}")
+
             proxys = getsubscription(server_info)
 
             try:
@@ -244,8 +328,7 @@ def main():
                 logger.info("使用第一个vmess地址")
                 updatecfg(proxys["vmess"][0][1])
             else:
-                logger.info("测试连接延时:")
-                logger.info(pprint.pformat(speed_sorted))
+                logger.info("测试连接延时:\n" + pprint.pformat(speed_sorted))
                 updatecfg(speed_sorted[0][1])
 
             if subprocess.Popen == type(v2ray_process):
@@ -254,15 +337,16 @@ def main():
                 v2ray_process = v2ray(v2ray_config)
 
             last_server_info = server_info
+        
+        check_subscription()
 
         days = 60*60 * UPDATE_INTERVAL
         time.sleep(days)
-        T = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        logger.info(f"{T}: 更新节点信息")
+        logger.info(f"更新节点信息")
 
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        logger.info(".....")
+        logger.info("Ctrl+C exit...")
