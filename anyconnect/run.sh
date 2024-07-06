a64e8cb92ade4fad733a063311a690c7f2672f22
calllivecn <calllive@outlook.com>
2024-07-06 22:29:43 +0800

    删除anyconnect

--- anyconnect/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# date 2018-10-26 10:18:32
-# author calllivecn <calllivecn@outlook.com>
-
-sudo xhost +
-
-
-
-docker run -it --rm --cap-add NET_ADMIN -e DISPLAY=$DISPLAY -v /etc/localtime:/etc/localtime:ro -v /tmp/.X11-unix:/tmp/.X11-unix docker-run-gui /opt/cisco/anyconnect/bin/vpnui
