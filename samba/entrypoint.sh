fa7642aa08d481c7e96056f318ef69924d119f7b
calllivecn <calllivecn@outlook.com>
2024-07-07 17:58:19 +0800

    update

--- samba/entrypoint.sh
+++ samba/entrypoint.sh
@@ -22,6 +22,6 @@ echo -e "${PW}\n${PW}" |pdbedit -t -a -u root
 # v4.15.7 之前
 #exec smbd -FS --no-process-group -d ${LOGLEVEL} -s /etc/samba/smb.conf -p 445
 
-# v4.15.7 之前
+# v4.15.7 之后
 exec smbd -F --no-process-group -d ${LOGLEVEL} -s /etc/samba/smb.conf -p 1445
 
