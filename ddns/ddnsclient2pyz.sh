629539d12b1124182faf256e6909dcf4a36a49b4
calllivecn <calllivecn@outlook.com>
2025-07-25 21:54:15 +0800

    客户端添加 *.pyz 打包方式

---
+++ ddns/ddnsclient2pyz.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# date 2022-03-15 16:59:33
+# author calllivecn <calllivecn@outlook.com>
+
+
+CWD=$(pwd -P)
+TMP1=$(mktemp -d -p "$CWD")
+
+DEPEND_CACHE="${CWD}/depend-cache"
+
+PYROUTE2=$(grep "pyroute2==" requirements.txt)
+
+if [ -d "$DEPEND_CACHE" ];then
+	echo "使用depend-cache ~"
+	(cd "$DEPEND_CACHE";cp -rv . "$TMP1")
+else
+	mkdir -v "${DEPEND_CACHE}"
+	pip3 install --no-compile --target "$DEPEND_CACHE" "$PYROUTE2"
+	(cd "$DEPEND_CACHE";cp -rv . "$TMP1")
+fi
+
+clean(){
+	echo "clean... ${TMP1} ${TMP2}"
+	rm -rf "${TMP1}" "${TMP2}"
+	echo "done"
+}
+
+trap clean SIGINT SIGTERM EXIT ERR
+
+(cd src/;cp -rv . "${TMP1}/")
+
+rm -rf "${TMP1}/__pycache__/"
+
+python3 -m zipapp --python '/usr/bin/env python3' --main ddnsclient:main --compress --output ddnsclient.pyz "${TMP1}" 
