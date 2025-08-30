c7aab71b88014be17819ee9e67ada5cc6b518ef9
calllivecn <calllivecn@outlook.com>
2025-08-30 15:50:13 +0800

    update

--- alpine-v2ray/run-build.sh
+++ alpine-v2ray/run-build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ash
 # date 2022-02-14 22:29:15
 # author calllivecn <calllivecn@outlook.com>
 
@@ -12,9 +12,9 @@ v2ray_path="/v2ray"
 #wget -O /usr/local/bin/jsonfmt.py https://github.com/calllivecn/mytools/raw/master/jsonfmt.py
 
 download(){
-tag_name_json="/tmp/tag_name.json"
+	tag_name_json="/tmp/tag_name.json"
 
-wget -O $tag_name_json https://api.github.com/repos/v2ray/v2ray-core/releases/latest
+	wget -O $tag_name_json https://api.github.com/repos/v2fly/v2ray-core/releases/latest
 
 
 cat > /tmp/json_tag_name.py<<EOF
@@ -23,12 +23,12 @@ with open("$tag_name_json") as f:
    data = json.load(f)
 print(data["tag_name"])
 EOF
-
-tag_name=$(python3 /tmp/json_tag_name.py)
-
-rm -v /tmp/json_tag_name.py
-
-wget -O "/tmp/$v2ray_pkg" https://github.com/v2fly/v2ray-core/releases/download/$tag_name/$v2ray_pkg
+	
+	tag_name=$(python3 /tmp/json_tag_name.py)
+	
+	rm -v /tmp/json_tag_name.py
+	
+	wget -O "/tmp/$v2ray_pkg" https://github.com/v2fly/v2ray-core/releases/download/$tag_name/$v2ray_pkg
 
 }
 
