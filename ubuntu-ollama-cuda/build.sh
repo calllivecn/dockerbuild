c41e5e6d2783b787a6eaf28ea89479ed18c3a834
calllivecn <calllivecn@outlook.com>
2026-08-30 14:49:55 +0800

    save

--- ubuntu-ollama-cuda/build.sh
+++ ubuntu-ollama-cuda/build.sh
@@ -2,10 +2,14 @@
 # date 2018-08-28 15:27:02
 # author calllivecn <calllivecn@outlook.com>
 
-. ../libbuild-depends.sh
+#. ../libbuild-depends.sh
+#
+#if [ -n $NO_CACHE ];then
+#	podman build --no-cache --build-arg https_proxy="http://10.1.3.1:9999" -t ${IMAGE_NAME} .
+#else
+#	podman build --build-arg https_proxy="http://10.1.3.1:9999" -t ${IMAGE_NAME} .
+#fi
 
-if [ -n $NO_CACHE ];then
-	podman build --no-cache --build-arg https_proxy="http://10.1.3.1:9999" -t ${IMAGE_NAME} .
-else
-	podman build --build-arg https_proxy="http://10.1.3.1:9999" -t ${IMAGE_NAME} .
-fi
+
+podman build -f Dockerfile-download -t ubuntu-ollama-cuda .
+
