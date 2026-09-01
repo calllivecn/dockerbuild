d83ec2fe1a72f1ffa7e58226ec375c13476a8b0f
calllivecn <calllive@outlook.com>
2026-09-01 18:40:49 +0800

    更新获取linux 本地 ip 的脚本, 排除 lo 接口

--- ddns/getipcmd/linux-ipv4.sh
+++ ddns/getipcmd/linux-ipv4.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
 #ip -4 route get 1.1.1.1 | grep -oP "(?<=1.1.1.1 via )(\d{1,3}\.){1,3}\d{1,3} (?=dev )"
+
 iface=$(ip -4 route get 1.1.1.1 |awk '{print $5}')
 
+if [ "$iface"x = "lo"x ];then
+	exit 1
+fi
 
 ip -4 addr show "$iface" |awk '$0~/inet / {print $2}'|awk -F'/' '{print $1}'
 
--- ddns/getipcmd/linux-ipv6.sh
+++ ddns/getipcmd/linux-ipv6.sh
@@ -2,6 +2,10 @@
 
 iface=$(ip -6 route get 2400:3200:baba::1 |awk '{print $7}')
 
+if [ "$iface"x = "lo"x ];then
+	exit 1
+fi
+
 ip -6 addr show "$iface" |awk '$0~/mngtmpaddr/{print $2}' |awk -F'/' '{print $1}'
 
 
